d8a4fa736cd1a75a46f53ca92234c4013ff498cb
Oleksandra <mremarka314@gmail.com>
2023-12-18 17:55:03 +0200

    add load data to table

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+import sqlite3
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow,
-    QTableWidget,
+    QTableWidget, QTableWidgetItem,
     QLabel,
 )
 from PyQt6.QtGui import QAction
@@ -24,12 +25,26 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("id", "name", "course", "phone"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        pass
+        connection = sqlite3.connect("database.db")
+        sql_query = "SELECT * FROM students"
+        result = connection.execute(sql_query)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(
+                    row_number, 
+                    column_number, 
+                    QTableWidgetItem(str(data)
+                ))
+        connection.close()
+        
 
 app = QApplication(sys.argv)
 app_window = MainWindow()
 app_window.show()
+app_window.load_data()
 sys.exit(app.exec())
